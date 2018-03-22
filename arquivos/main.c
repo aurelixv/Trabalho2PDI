e6823b0b1dc863bac1dc4ca39b206a719208667d
Aurélio Vinícius <aurelio@alunos.utfpr.edu.br>
2018-03-21 22:41:03 -0300

    esqueleto main e mudanças no makefile

--- arquivos/main.c
+++ arquivos/main.c
@@ -3,9 +3,34 @@
 #include <time.h>
 #include "pdi.h"
 
+#define IMAGEM "Exemplos/a01 - Original.bmp"
+
 int main() {
     
-    printf("Hello, world!");
+    Imagem *imagem;
+    
+    //Abre a imagem a ser borrada
+    printf("A carregar a imagem [ %s ]... ", IMAGEM);
+    imagem = abreImagem(IMAGEM, 3);
+    if(!imagem) {
+            printf("\nERRO: A imagem não pode ser aberta.\n");
+            exit(1);
+    }
+    printf("Sucesso!\n");
+    
+    //Funções que borram a imagem
+    //TODO: Algoritmo ingênuo
+    
+    //TODO: Filtro separável
+    
+    //TODO: Algoritmo com imagens integrais
+    
+    
+    //Libera a memória alocada antes de terminar
+    printf("Liberando memória...\n");
+    destroiImagem(imagem);
+    
+    printf("Encerrando...\n");
     
     return 0;
 }
