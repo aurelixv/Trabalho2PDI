3f9cadb15f8a01074b9e56541183faf19027f3aa
Lincoln <linkskill@gmail.com>
2018-03-23 22:24:56 -0300

    implementando filtro separavel

--- arquivos/main.c
+++ arquivos/main.c
@@ -100,16 +100,14 @@ void separavel(Imagem *in, Imagem *out, int a, int l) {
     int bordery = a/2;
     int borderx = l/2;
     float soma = 0.0f;
-    //TODO: ALOCAR NOVA IMAGEM ANTES DA IGUALDADE
-    Imagem *buffer = in;
+    Imagem *buffer = criaImagem(in->largura, in->altura, in->n_canais);
     
     /** Filtro horizontal **/
     for(canal = 0; canal < in->n_canais; canal++) {
         for(y = 0; y < in->altura; y++) {
             for(x = borderx; x < in->largura - borderx; x++) {
                 for(i = x - borderx; i <= x + borderx; i++) {
-                    //TODO: MUDAR OPERACAO
-                    soma += buffer->dados[canal][y][i];
+                    soma += in->dados[canal][y][i];
                 }
                 buffer->dados[canal][y][x] = soma/l;
                 soma = 0.0f;
@@ -122,16 +120,13 @@ void separavel(Imagem *in, Imagem *out, int a, int l) {
         for(x = 0; x < in->largura; x++) {
             for(y = bordery; y < in->altura - bordery; y++) {
                 for(i = y - bordery; i <= y + bordery; i++) {
-                    //TODO: MUDAR OPERACAO
                     soma += buffer->dados[canal][i][x];
                 }
-                buffer->dados[canal][y][x] = soma/a;
+                out->dados[canal][y][x] = soma/a;
                 soma = 0.0f;
             }
         }
     }
-    
-    *out = *buffer;
 
     printf("[\x1b[32m OK \x1b[0m]\n");
 }
